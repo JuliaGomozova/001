2f508711de5d2c772787232e2fb0399a319d86fe
Julia Gomozova <sunrain@yandex.ru>
2023-01-27 23:10:03 +0300

    find length of rezult array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"one", "two", "three", "four", "five", "six", "seven", "-1"};
+
+int resultArraySize = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length == 3) 
+        resultArraySize++;
+}
+
+string[] resultArray =  new string[resultArraySize];
